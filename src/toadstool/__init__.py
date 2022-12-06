a1fe7937fb6a13192fc31f451a9a683745defa46
acalejos <andres.alejos@gmail.com>
2022-12-05 23:58:10 -0500

    setup init for many loaders

---
+++ src/toadstool/__init__.py
@@ -0,0 +1,20 @@
+import sys
+import logging
+from importlib import import_module
+
+LOG = logging.getLogger("toadstool")
+all_loaders= [
+    ('toadstool.loaders.gql_loader','GqlLoader','gql')
+]
+active_loaders = []
+
+for loader_mod, loader_class, extra_option in all_loaders:
+    try:
+        module = import_module(loader_mod)
+        cls = getattr(module,loader_class)
+    except (ImportError, ModuleNotFoundError):
+        logging.info(f"Running without {extra_option} support. Install with `pip install {__name__}[{extra_option}]")
+    else:
+        active_loaders.append(cls)
+
+sys.meta_path += active_loaders

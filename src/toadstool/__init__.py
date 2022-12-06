126a5c52a63fc079faf59de302d96a09aacbae42
acalejos <andres.alejos@gmail.com>
2022-12-05 23:59:59 -0500

    update logging

--- src/toadstool/__init__.py
+++ src/toadstool/__init__.py
@@ -13,7 +13,7 @@ for loader_mod, loader_class, extra_option in all_loaders:
         module = import_module(loader_mod)
         cls = getattr(module,loader_class)
     except (ImportError, ModuleNotFoundError):
-        logging.info(f"Running without {extra_option} support. Install with `pip install {__name__}[{extra_option}]")
+        LOG.info(f"Running without {extra_option} support. Install with `pip install {__name__}[{extra_option}]")
     else:
         active_loaders.append(cls)
 
